e9b1456cdc8a526e336c9cdc80547ccf2b4268a3
BaiYanchen <2786660867@qq.com>
2025-04-11 14:57:26 +0800

    fix: 🐛 修复打包入口错误

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,7 +6,7 @@ const isDev = process.env.NODE_ENV === 'development'
 export default defineConfig({
   tsconfig: './tsconfig.json',
   entry: {
-    index: 'src/index.ts',
+    index: 'core/index.ts',
   },
   external: [
     '@rsbuild/core',
